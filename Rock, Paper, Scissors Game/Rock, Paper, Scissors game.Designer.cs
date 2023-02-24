28f83529a387540df644930e03d57bbb259a68fd
Brian Nicewander <Brian_Nicewander@hotmail.com>
2023-02-24 09:38:11 -0600

    Add project files.

---
+++ Rock, Paper, Scissors Game/Rock, Paper, Scissors game.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Rock__Paper__Scissors_Game
+{
+    partial class rockPaperScissorsGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPick = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnPlayGmae = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.lblWonTheMost = new System.Windows.Forms.Label();
+            this.lblPickNumber = new System.Windows.Forms.Label();
+            this.lblResultsShow = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblWonTheMostShow = new System.Windows.Forms.Label();
+            this.lblUserPickShow = new System.Windows.Forms.Label();
+            this.lblComputerPIckShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPick
+            // 
+            this.lblPick.AutoSize = true;
+            this.lblPick.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPick.Location = new System.Drawing.Point(65, 9);
+            this.lblPick.Name = "lblPick";
+            this.lblPick.Size = new System.Drawing.Size(274, 16);
+            this.lblPick.TabIndex = 1;
+            this.lblPick.Text = "Please pick a number between 1 and 5";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(136, 63);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // btnPlayGmae
+            // 
+            this.btnPlayGmae.Location = new System.Drawing.Point(146, 98);
+            this.btnPlayGmae.Name = "btnPlayGmae";
+            this.btnPlayGmae.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayGmae.TabIndex = 3;
+            this.btnPlayGmae.Text = "Play Game";
+            this.btnPlayGmae.UseVisualStyleBackColor = true;
+            this.btnPlayGmae.Click += new System.EventHandler(this.btnPlayGmae_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(126, 137);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(126, 18);
+            this.lblResults.TabIndex = 4;
+            this.lblResults.Text = "Result of game";
+            // 
+            // lblWonTheMost
+            // 
+            this.lblWonTheMost.AutoSize = true;
+            this.lblWonTheMost.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWonTheMost.Location = new System.Drawing.Point(120, 273);
+            this.lblWonTheMost.Name = "lblWonTheMost";
+            this.lblWonTheMost.Size = new System.Drawing.Size(132, 16);
+            this.lblWonTheMost.TabIndex = 5;
+            this.lblWonTheMost.Text = "Who won the most";
+            // 
+            // lblPickNumber
+            // 
+            this.lblPickNumber.AutoSize = true;
+            this.lblPickNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPickNumber.Location = new System.Drawing.Point(43, 34);
+            this.lblPickNumber.Name = "lblPickNumber";
+            this.lblPickNumber.Size = new System.Drawing.Size(344, 16);
+            this.lblPickNumber.TabIndex = 0;
+            this.lblPickNumber.Text = "1=Rock, 2=Paper, 3=Scissors, 4=Lizard, 5=Spock";
+            // 
+            // lblResultsShow
+            // 
+            this.lblResultsShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultsShow.Location = new System.Drawing.Point(81, 227);
+            this.lblResultsShow.Name = "lblResultsShow";
+            this.lblResultsShow.Size = new System.Drawing.Size(205, 22);
+            this.lblResultsShow.TabIndex = 7;
+            this.lblResultsShow.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(111, 409);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 35);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset the game count";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(192, 409);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 35);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblWonTheMostShow
+            // 
+            this.lblWonTheMostShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWonTheMostShow.Location = new System.Drawing.Point(111, 301);
+            this.lblWonTheMostShow.Name = "lblWonTheMostShow";
+            this.lblWonTheMostShow.Size = new System.Drawing.Size(156, 86);
+            this.lblWonTheMostShow.TabIndex = 6;
+            // 
+            // lblUserPickShow
+            // 
+            this.lblUserPickShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblUserPickShow.Location = new System.Drawing.Point(111, 165);
+            this.lblUserPickShow.Name = "lblUserPickShow";
+            this.lblUserPickShow.Size = new System.Drawing.Size(154, 20);
+            this.lblUserPickShow.TabIndex = 10;
+            this.lblUserPickShow.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblComputerPIckShow
+            // 
+            this.lblComputerPIckShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblComputerPIckShow.Location = new System.Drawing.Point(111, 196);
+            this.lblComputerPIckShow.Name = "lblComputerPIckShow";
+            this.lblComputerPIckShow.Size = new System.Drawing.Size(154, 20);
+            this.lblComputerPIckShow.TabIndex = 11;
+            this.lblComputerPIckShow.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rockPaperScissorsGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 457);
+            this.Controls.Add(this.lblComputerPIckShow);
+            this.Controls.Add(this.lblUserPickShow);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblResultsShow);
+            this.Controls.Add(this.lblWonTheMostShow);
+            this.Controls.Add(this.lblWonTheMost);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.btnPlayGmae);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblPick);
+            this.Controls.Add(this.lblPickNumber);
+            this.Name = "rockPaperScissorsGame";
+            this.Text = "Rock, Paper, Scissors Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPick;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnPlayGmae;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Label lblWonTheMost;
+        private System.Windows.Forms.Label lblPickNumber;
+        private System.Windows.Forms.Label lblResultsShow;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWonTheMostShow;
+        private System.Windows.Forms.Label lblUserPickShow;
+        private System.Windows.Forms.Label lblComputerPIckShow;
+    }
+}
+
---
+++ Rock, Paper, Scissors Game/Rock, Paper, Scissors game.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock__Paper__Scissors_Game
+{
+
+    public partial class rockPaperScissorsGame : Form
+    {
+        Random rand = new Random(); //makes a random number
+        private int computerPick = 0; // holds the user pick
+        private int userPick = 0; //holds the user pick
+        private int computerTotal = 0; // holds how many times the computer won
+        private int userTotal = 0; // holds how many times the user won
+        private int ties = 0; //holds how many times the user nad computer tied
+        private int userPickTwo; // the holder for the user pick after the return
+        private int computerPickTwo; //the new holder for the computer pick after the return
+
+        public rockPaperScissorsGame()
+        {
+            InitializeComponent();
+        }
+
+        public int userPickNumber(int userPick)
+        { //returns the userpick to userPickNumber
+            return userPick;
+        }
+
+        public int computerPickNumber(int ComputerPick)
+        {//returns the computerPIck to computerPickNumber
+            return computerPick = rand.Next(1, 6);
+        }
+
+        private void btnPlayGmae_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNumber.Text, out userPick)) //turns what the user put in into a int
+          
+                userPickTwo = userPickNumber(userPick);//user pick after return
+
+            if (userPickTwo >= 1 && userPick <= 6)//checks what the user puts in 
+            {
+                //puts what the user picked on the screen
+                if (userPickTwo == 1)
+                    lblUserPickShow.Text = "You picked Rock";
+                else if (userPickTwo == 2)
+                    lblUserPickShow.Text = "You picked Paper";
+                else if (userPickTwo == 3)
+                    lblUserPickShow.Text = "You picked Scissors";
+                else if (userPickTwo == 4)
+                    lblUserPickShow.Text = "You picked Lizard";
+                else if (userPickTwo == 5)
+                    lblUserPickShow.Text = "You picked Spock";
+
+            computerPickTwo = computerPickNumber(computerPick);//computer pick after return
+
+            //puts what the user picked on the screen
+            if (computerPickTwo == 1)
+                lblComputerPIckShow.Text = "The computer picked Rock";
+            else if (computerPickTwo == 2)
+                lblComputerPIckShow.Text = "The computer picked Paper";
+            else if (computerPickTwo == 3)
+                lblComputerPIckShow.Text = "The computer picked Scissors";
+            else if (computerPickTwo == 4)
+                lblComputerPIckShow.Text = "The computer picked Lizard";
+            else if (computerPickTwo == 5)
+                lblComputerPIckShow.Text = "The computer picked Spock";
+
+            // This is when the player wins and the computer loses 
+            if (userPickTwo == 3 && computerPickTwo == 2)
+            {
+                txtNumber.Text = ""; //clears the text box 
+                lblResultsShow.Text = "Scissors cuts paper 'you won'";//show the user the result
+                userTotal++; //adds one to the user win total
+                userPickTwo = 0; //makes it where the user has to pick a new number
+                }
+            else if (userPick == 2 && computerPick == 1)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Paper covers rock 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 1 && computerPick == 4)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Rock crushes lizard 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 4 && computerPick == 5)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Lizard posions spock 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 5 && computerPick == 3)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Spock smashes scissors 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 3 && computerPick == 4)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Scissors decapitates lizard 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 4 && computerPick == 2)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Lizard eats paper 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+
+                }
+            else if (userPick == 2 && computerPick == 5)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Paper disproves spock 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 5 && computerPick == 1)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Spock vaporizes rock 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 1 && computerPick == 3)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Rock crushes scissors 'you won'";
+                userTotal++;
+                userPickTwo = 0;
+                }
+
+            // This is when the computer wins and the player loses 
+            else if (userPickTwo == 2 && computerPickTwo == 3)
+            {
+                txtNumber.Text = "";//clears the text box 
+                lblResultsShow.Text = "Scissors cuts paper 'you lost'";//show the user the result
+                computerTotal++; // adds one to the computer win total
+                userPickTwo = 0; //makes it where the user has to pick a new number
+                }
+            else if (userPick == 1 && computerPick == 2)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Paper covers rock 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 4 && computerPick == 1)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Rock crushes lizard 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 5 && computerPick == 4)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Lizard posions spock 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 3 && computerPick == 5)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Spock smashes scissors 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 4 && computerPick == 3)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Scissors decapitates lizard 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 2 && computerPick == 4)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Lizard eats paper 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 5 && computerPick == 2)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Paper disproves spock 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 1 && computerPick == 5)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Spock vaporizes rock 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 3 && computerPick == 1)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "Rock crushes scissors 'you lost'";
+                computerTotal++;
+                userPickTwo = 0;
+                }
+
+            // This is for ties
+            else if (userPick == 1 && computerPick == 1)
+            {
+                txtNumber.Text = "";//clears the text box 
+                lblResultsShow.Text = "It is a tie "; //show the user the result
+                ties++; // adds one to the tie total
+                userPickTwo = 0; //makes it where the user has to pick a new number
+                }
+            else if (userPick == 2 && computerPick == 2)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "It is a tie ";
+                ties++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 3 && computerPick == 3)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "It is a tie ";
+                ties++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 4 && computerPick == 4)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "It is a tie ";
+                ties++;
+                userPickTwo = 0;
+                }
+            else if (userPick == 5 && computerPick == 5)
+            {
+                txtNumber.Text = "";
+                lblResultsShow.Text = "It is a tie ";
+                ties++;
+                userPickTwo = 0;
+                }
+            // show sthe output of how many times the user won, the computer won, and ties
+            lblWonTheMostShow.Text = "You won " + userTotal + " times" + "\n\nThe computer won " + computerTotal + " times" + "\n\nYou tied " + ties + " times";
+        }else{
+                MessageBox.Show("Please pick a number between 1 and 5");//If the user picks something other then 1 through 5
+        }
+      }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {//reset the score back to 0 and clears the labels
+            txtNumber.Text = "";
+            lblUserPickShow.Text = "";
+            lblComputerPIckShow.Text = "";
+            lblWonTheMostShow.Text = "";
+            lblResultsShow.Text = "";
+            computerTotal = 0;
+            userTotal = 0;
+            ties = 0;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {//This closes the program 
+            this.Close();
+        }
+    }
+}
